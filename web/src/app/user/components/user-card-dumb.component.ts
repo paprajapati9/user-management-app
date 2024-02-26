2b23546a36b37fbbea2b2b3fa4ec73e5615604dd
Pankaj Prajapati <pankajprajapati@Pankajs-MacBook-Pro.local>
2024-02-26 13:05:45 +0530

    minor change

--- web/src/app/user/components/user-card-dumb.component.ts
+++ web/src/app/user/components/user-card-dumb.component.ts
@@ -1,11 +1,15 @@
 import { Component, Input } from "@angular/core";
 import { User } from "../types/user.type";
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
     selector: 'app-user-card-dumb',
     templateUrl: './user-card-dumb.component.html',
     styleUrls: ['./user-card-dumb.component.css'],
     standalone: true,
+    imports: [
+        MatIconModule
+    ]
 })
 export class UserCardDumbComponent {
     @Input() user!: User
